42fcd0e558e12a6db3273e0876dc6a7aa22673c9
isabelaCosta95 <isabelaovc95@gmail.com>
2025-02-12 20:39:37 -0300

    Primeiro Exercício

---
+++ primeiralista/resources/views/exer1.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('conteudo')
+
+<form method="post" action="/exer1resp">
+    @csrf
+    <div class="mb-3">
+        <label for="valor1" class="form-label">Informe o Valor 1</label>
+        <input type="number" id="valor1" name="valor1" class="form-control" required="">
+    </div>
+
+    <div class="mb-3">
+        <label for="valor2" class="form-label">Informe o Valor 2</label>
+        <input type="number" id="valor2" name="valor2" class="form-control" required="">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+    @isset($soma)
+        <p>O Valor da Soma é {{ $soma }}</p>
+    @endisset
+
+@endsection
